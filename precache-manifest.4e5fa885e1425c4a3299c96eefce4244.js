abc64be32c09b087fcc82d823ceeee2d3ca18766
Tan1107 <tannguyen1107.bku@gmail.com>
2020-06-13 08:05:56 +0700

    Updates

---
+++ precache-manifest.4e5fa885e1425c4a3299c96eefce4244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1f2e487ebbd02bf5433fe18298ccf4",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8b7781defc2135c92f91",
+    "url": "/weather-app/static/css/main.4567fca2.chunk.css"
+  },
+  {
+    "revision": "f228d2cc826ea56229ee",
+    "url": "/weather-app/static/js/2.a9cdc25a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.a9cdc25a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7781defc2135c92f91",
+    "url": "/weather-app/static/js/main.f29fe9f0.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  }
+]);
